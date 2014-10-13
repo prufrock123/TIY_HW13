8bc51ac72c5bbdd7989a58533985e6d20b0ebf59
Julie Rich <julierich1211@yahoo.com>
2014-10-13 09:32:04 -0500

    julie work

--- js/app.js
+++ js/app.js
@@ -10,14 +10,80 @@ function customForEach(array, callback) {
 
 // 2. 
 
-function customMap(array, callback) {
-    "use strict";
-    var newArray = [];
-    for (var i = 0; i < array.length; i++) {
-        newArray.push(callback(array[i], i, array));
-    }
-    return newArray;
+var students = [{
+    firstname: "Julie",
+    lastname: "Rich",
+    age: "36",
+    gender: "female"
+}, {
+    firstname: "Matt",
+    lastname: "Keas",
+    age: "26",
+    gender: "male"
+}, {
+    firstname: "Ken",
+    lastname: "McLeod",
+    age: "40",
+    gender: "male"
+}, {
+    firstname: "Phil",
+    lastname: "Warner",
+    age: "25",
+    gender: "male"
+}, {
+    firstname: "Tawfiq",
+    lastname: "Momin",
+    age: "32",
+    gender: "male"
+}, {
+    firstname: "Ryan",
+    lastname: "Mora",
+    age: "35",
+    gender: "male"
+}, {
+    firstname: "Shawn",
+    lastname: "Miller",
+    age: "33",
+    gender: "male"
+}];
+
+function filter(array, test) {
+  var passed = [];
+  for (var i = 0; i < array.length; i++) {
+    if (test(array[i]))
+      passed.push(array[i]);
+  }
+  return passed;
 }
+console.log(filter(students, function(person) {
+  return person.age > 30 && person.age < 50;
+}));
+function map(array, transform) {
+  var mapped = [];
+  for (var i = 0; i < array.length; i++)
+    mapped.push(transform(array[i]));
+  return mapped;
+}
+
+var sub = students.filter(function(person) {
+  return person.gender - person.age > 20;
+});
+console.log(map(sub, function(person) {
+  return person.firstname;
+}));
+
+
+// 2.5
+function reduce(array, combine, start) {
+  var current = start;
+  for (var i = 0; i < array.length; i++)
+    current = combine(current, array[i]);
+  return current;
+}
+
+console.log(reduce([1, 2, 3, 4], function(a, b) {
+  return a + b;
+}, 0));
 // 3.
 var stooges = [{
     name: "moe",
