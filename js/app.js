acec72fe59cab3e5eda3b7eea0e6a5aa85d9ffc2
prufrock123 <shawnmllr86@gmail.com>
2014-10-13 17:22:48 -0500

    Adds shawn's functions and adds a script tag for the original app.js file in the index.html file.

--- js/app.js
+++ js/app.js
@@ -1,119 +1,181 @@
-// 1.
-
+// 1. Custom forEach function
 function customForEach(array, callback) {
-    "use strict";
-    for (var i = 0; i < array.length; i++) {
-        callback(array[i], i, array);
+        "use strict";
+        for (var i = 0; i < array.length; i++) {
+            callback(array[i], i, array);
+        }
     }
+    //PUSH?PULL TEST SHAWN
+
+// 2. Map, reduce and filter functions
+function customMap(array, callback) {
+    "use strict";
+    var newArray = [];
+    for (var i = 0; i < array.length; i++) {
+        newArray.push(callback(array[i], i, array));
+    }
+    return newArray;
 }
-//PUSH?PULL TEST SHAWN
 
-// 2. 
+function customReduce(array, hollaback, initialVal) { // she ain't not hollaback girl.
+    "use strict";
 
+    var prev = initialVal;
 
-function customMap(array, callback){
+    if (initialVal) {
+        array.unshift(initialVal);
+    };
+
+    customForEach(array, function(element, index, array) {
+        if (index === 0) {
+        	prev = element;
+            return;
+        } else {
+            prev = hollaback(prev, element, index, array);
+        }
+    });
+    return prev;
+}
+
+function customFilter(array, callback) {
+	"use strict"
+	var emptyArray = [];
+	customForEach(array, function(element, index, array){
+		if (callback(element, index, array)){
+			emptyArray.push(element);
+		};
+	});
+	return emptyArray;
+}
+
+// 3. customPluck function
+function customPluck(array, property){
 	"use strict";
-	var newArray = [];
-	for (var i = 0; i < array.length; i++){
-		newArray.push(callback(array[i], i, array));
-	}
-	return newArray;
+	var emptyArray = [];
+	customForEach(array, function(element, i, array){
+		emptyArray.push(element[property]);
+	});
+	return emptyArray;
 }
 
-
-
-function reduce(array, hollaback){ // she ain't not hollaback girl.
+// 4. customReject function
+function customReject(array, callback){
 	"use strict";
-	var prev = null;
-	if (array.length === 1){
-		return 
-	}
-
+	var emptyArray = [];
+	customForEach(array, function(element, i, array){
+		if (callback(element, i, array)){
+			return;
+		} else {
+			emptyArray.push(element);
+		}
+	});
+	return emptyArray;
 }
 
-function filter(array hollaback){
-
+// 5. customFind function
+function customFind(array, callback){
+	"use strict";
+	var result;
+	customForEach(array, function(e, i, array){
+		if ( result === undefined && callback(e, i, array) ){
+			result = e;
+		}
+	});
+	return result;
 }
 
+// 6. customWhere function
+function where(array, properties){
+	"use strict";
+	var emptyArray = [];
+	customForEach(array, function(e, i, array){
+		properties
+	});
+}
 
+// Julie's Work
+// 
 var students = [{
-    firstname: "Julie",
-    lastname: "Rich",
-    age: 36,
-    gender: "female"
-}, {
-    firstname: "Matt",
-    lastname: "Keas",
-    age: 26,
-    gender: "male"
-}, {
-    firstname: "Ken",
-    lastname: "McLeod",
-    age: 40,
-    gender: "male"
-}, {
-    firstname: "Melissa",
-    lastname: "Swink",
-    age: 21,
-    gender: "female"
-}, {
-    firstname: "Tawfiq",
-    lastname: "Momin",
-    age: 32,
-    gender: "male"
-}, {
-    firstname: "Landon",
-    lastname: "Rich",
-    age: 9,
-    gender: "male"
-}, {
-    firstname: "Shawn",
-    lastname: "Miller",
-    age: 33,
-    gender: "male"
+	    firstname: "Julie",
+	    lastname: "Rich",
+	    age: 36,
+	    gender: "female"
+	}, {
+	    firstname: "Matt",
+	    lastname: "Keas",
+	    age: 26,
+	    gender: "male"
+	}, {
+	    firstname: "Ken",
+	    lastname: "McLeod",
+	    age: 40,
+	    gender: "male"
+	}, {
+	    firstname: "Melissa",
+	    lastname: "Swink",
+	    age: 21,
+	    gender: "female"
+	}, {
+	    firstname: "Tawfiq",
+	    lastname: "Momin",
+	    age: 32,
+	    gender: "male"
+	}, {
+	    firstname: "Landon",
+	    lastname: "Rich",
+	    age: 9,
+	    gender: "male"
+	}, {
+	    firstname: "Shawn",
+	    lastname: "Miller",
+	    age: 33,
+	    gender: "male"
 }];
-function forEach(list, callback){
-    for (var i =0, len = list.length; i<len; i++){
+
+function forEach(list, callback) {
+    for (var i = 0, len = list.length; i < len; i++) {
         callback(list[i], i, list);
     }
 }
+
 function filter(list, callback) {
-  var passed = [];
-  forEach(list, function(val, i, arr) {
-    if (callback(val, i, arr)){
-      passed.push(val);
-    }
-  });
-  return passed;
+    var passed = [];
+    forEach(list, function(val, i, arr) {
+        if (callback(val, i, arr)) {
+            passed.push(val);
+        }
+    });
+    return passed;
 }
 console.log(filter(students, function(person) {
-  return person.age < 20;
+    return person.age < 20;
 }));
+
 function map(list, callback) {
-  var mapped = [];
-  forEach(list, function(val, i, arr) {
-    mapped.push(callback(val, i, arr));
-});
-  return mapped;
+    var mapped = [];
+    forEach(list, function(val, i, arr) {
+        mapped.push(callback(val, i, arr));
+    });
+    return mapped;
 }
 var sub = students.filter(function(person) {
-  return person.gender - person.age > 20;
+    return person.gender - person.age > 20;
 });
 console.log(map(sub, function(person) {
-  return person.firstname;
+    return person.firstname;
 }));
 
 
 // 2.5
 function reduce(array, combine, start) {
-  var current = start;
-  for (var i = 0; i < array.length; i++)
-    current = combine(current, array[i]);
-  return current;
+    var current = start;
+    for (var i = 0; i < array.length; i++)
+        current = combine(current, array[i]);
+    return current;
 }
 
 console.log(reduce([1, 2, 3, 4], function(a, b) {
-  return a + b;
+    return a + b;
 }, 0));
 // 3.
 var stooges = [{
